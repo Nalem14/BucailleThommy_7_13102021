43d00163e7774a2671a68fc856a414a85265dbbe
Thommy Bucaille <nalem.14360@gmail.com>
2021-10-13 13:15:09 +0200

    Init base files for NodeJS and Express

---
+++ app/routes/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../middleware/auth.middleware");
+const bouncer = require('express-bouncer')(5000, 900000, 3); // (min, max, attempts)
+
+// Configure spam-protection
+bouncer.whitelist.push('127.0.0.1'); // allow an IP address
+// give a custom error message
+bouncer.blocked = function (req, res, next, remaining) {
+    res.status(429).json({ error: "Vous avez effectué trop de requêtes. Ré-essayez dans " + remaining/1000 + " secondes." });
+};
+
+//router.use("/api/route", authMiddleware, myRoute);
+
+module.exports = router;

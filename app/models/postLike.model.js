8d438c3f000464b857fe27256ca82d8ce31c743f
Thommy Bucaille <nalem.14360@gmail.com>
2021-10-14 21:51:25 +0200

    Added PostLike Model

---
+++ app/models/postLike.model.js
@@ -0,0 +1,28 @@
+const bcrypt = require("bcrypt");
+
+/**
+ * Define the PostLike model
+ * @param {*} sequelize
+ * @param {*} DataTypes
+ * @returns PostLike model
+ */
+module.exports = function (sequelize, DataTypes) {
+  // Model Definition
+  const PostLike = sequelize.define("PostLike", {
+    user_id: {
+      type: DataTypes.INTEGER,
+      unique: false,
+      allowNull: false,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      unique: false,
+      allowNull: false,
+    }
+  });
+
+  // Reference Definition
+
+  // Return the PostLike model
+  return PostLike;
+};

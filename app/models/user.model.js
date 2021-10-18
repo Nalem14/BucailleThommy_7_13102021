c85c8497ba9a64c1939b24150f694b7b51a5f47c
Thommy Bucaille <nalem.14360@gmail.com>
2021-10-18 23:45:08 +0200

    Password strength and email encryption

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcrypt");
+const { pwnedPassword } = require('hibp');
+const Helper = require("../services/helper.service");
 
 /**
  * Define the User model
@@ -23,21 +25,41 @@ module.exports = function (sequelize, DataTypes) {
       allowNull: false,
       validate: {
         isEmail: true
+      },
+      set(value) {
+        this.setDataValue("email", Helper.encryptEmail(value));
+      },
+      get() {
+        return Helper.decryptEmail(rawValue);
       }
     },
     password: {
       type: DataTypes.STRING,
       unique: false,
       allowNull: false,
-      set(value) { // Bcrypt the password
+      set(value) {
+        // Check password strength
+        if(!(/^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{6,12}$/.test(value))) {
+            throw new Error('Le mot de passe doit contenir minimum 6 et maximum 12 caractères, incluant au moins 1 majuscule, 1 minuscule, un nombre et un caractère spécial.');
+        }
+
+        // Check pwned password
+        let nbPwned = await pwnedPassword(value);
+        if(nbPwned > 0) {
+          throw new Error("Ce mot de passe semble compromis.");
+        }
+
+        // Bcrypt the password
         bcrypt.hash(value, 10).then((hash) => {
           this.setDataValue("password", hash);
         });
-      },
-      validate: {
-        is: /^[0-9a-fA-F$#_]{64}$/i,
-        len: [6,12]
       }
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      unique: false,
+      allowNull: false,
+      defaultValue: false
     },
     avatar: {
       type: DataTypes.STRING,

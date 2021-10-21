dd689834e6d8480697e5b79db1fb2db148d36cb7
Bucaille Thommy <nalem.14360@gmail.com>
2021-10-21 17:04:47 +0200

    Add unique slug to community

--- app/controllers/community.controller.js
+++ app/controllers/community.controller.js
@@ -1,71 +1,88 @@
 const Helper = require("../helpers");
 const db = require("../models");
 
+/**
+ * Create a Community
+ * @param {*} req
+ * @param {*} res
+ * @returns response
+ */
+exports.create = async (req, res) => {
+  try {
+    if (!("title" in req.body) || !("about" in req.body))
+      throw new Error(
+        "Veuillez spécifier un titre et la description à propos de la communauté."
+      );
 
+    await db.Community.create({
+      title: req.body.title,
+      about: req.body.about,
+    });
+
+    return Helper.successResponse(req, res, {}, hateoas(req));
+  } catch (error) {
+    console.error(error);
+    return Helper.errorResponse(req, res, error.message);
+  }
+};
+
+/**
+ * Read all communities
+ * @param {*} req 
+ * @param {*} res 
+ * @returns response
+ */
 exports.readAll = async (req, res) => {
-    try {
-        let communities = await db.Community.findAll();
-        if(communities.length == 0) throw new Error("Aucune communauté.");
+  try {
+    let communities = await db.Community.findAll();
+    if (communities.length == 0) throw new Error("Aucune communauté.");
 
-        return Helper.successResponse(req, res, { communities }, hateoas(req));
-    } catch (error) {
-        console.error(error);
-        return Helper.errorResponse(req, res, error.message);
-    }
+    return Helper.successResponse(req, res, { communities }, hateoas(req));
+  } catch (error) {
+    console.error(error);
+    return Helper.errorResponse(req, res, error.message);
+  }
 };
 
+/**
+ * Read one Community by id
+ * @param {*} req 
+ * @param {*} res 
+ * @returns response
+ */
 exports.readOne = async (req, res) => {
-    try {
-        let community = await db.Community.findByPk(req.params.id);
-        if(community == null) throw new Error("Cette communauté n'existe pas.");
+  try {
+    let community = await db.Community.findByPk(req.params.id);
+    if (community == null) throw new Error("Cette communauté n'existe pas.");
 
-        return Helper.successResponse(req, res, { community }, hateoas(req));
-    } catch (error) {
-        console.error(error);
-        return Helper.errorResponse(req, res, error.message);
-    }
-};
-
-exports.add = async (req, res) => {
-    try {
-
-        if(!('title' in req.body) || !('about' in req.body))
-            throw new Error("Veuillez spécifier un titre et la description à propos de la communauté.");
-
-        await db.Community.create({
-            title: req.body.title,
-            about: req.body.about
-        });
-
-        return Helper.successResponse(req, res, {}, hateoas(req));
-    } catch (error) {
-        console.error(error);
-        return Helper.errorResponse(req, res, error.message);
-    }
+    return Helper.successResponse(req, res, { community }, hateoas(req));
+  } catch (error) {
+    console.error(error);
+    return Helper.errorResponse(req, res, error.message);
+  }
 };
 
 function hateoas(req) {
-    const baseUri = req.protocol + "://" + req.get("host");
-  
-    return [
-      {
-        rel: "readAll",
-        method: "GET",
-        title: "List all Communities",
-        href: baseUri + "/api/community",
-      },
-      {
-        rel: "readOne",
-        method: "GET",
-        title: "Read one Community",
-        href: baseUri + "/api/community/" + req.params.id,
-      },
-      {
-        rel: "add",
-        method: "POST",
-        title: "Add a Community",
-        href: baseUri + "/api/community",
-      },
-    ];
-  }
-  
+  const baseUri = req.protocol + "://" + req.get("host");
+
+  return [
+    {
+      rel: "create",
+      method: "POST",
+      title: "Create a Community",
+      href: baseUri + "/api/community",
+    },
+    {
+      rel: "readAll",
+      method: "GET",
+      title: "List all Communities",
+      href: baseUri + "/api/community",
+    },
+    {
+      rel: "readOne",
+      method: "GET",
+      title: "Read one Community",
+      href: baseUri + "/api/community/" + req.params.id||":id",
+    },
+  ];
+}
--- app/routes/community.routes.js
+++ app/routes/community.routes.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const { route } = require(".");
 const router = express.Router();
 const communityController = require("../controllers/community.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 
+router.post("/", authMiddleware, communityController.create);
 router.get("/", communityController.readAll);
 router.get("/:id", communityController.readOne);
-router.post("/", communityController.add);
 
 module.exports = router;

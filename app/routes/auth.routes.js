b3ba98378ee834a8c40e8c52623e11ccbd313e1b
Bucaille Thommy <nalem.14360@gmail.com>
2021-10-21 16:28:59 +0200

    Separate Auth and User controller

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/user.controller");
+const authController = require("../controllers/auth.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 
-router.post("/signup", userController.signup);
-router.post("/login", userController.login);
-router.get("/read", authMiddleware, userController.readMe);
-router.get("/export", authMiddleware, userController.exportMe);
-router.put("/update", authMiddleware, userController.update);
-router.delete("/delete", authMiddleware, userController.delete)
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+router.get("/read", authMiddleware, authController.readMe);
+router.get("/export", authMiddleware, authController.exportMe);
+router.put("/update", authMiddleware, authController.update);
+router.delete("/delete", authMiddleware, authController.delete)
 
 module.exports = router;

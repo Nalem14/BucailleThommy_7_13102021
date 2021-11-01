b18cf0b65c9ed7f37dfed1b9d86d5736d5a926a9
Bucaille Thommy <nalem.14360@gmail.com>
2021-11-01 23:38:00 +0100

    Added swagger Added Auth doc

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,6 +9,10 @@ const postRoutes = require("./post.routes");
 const commentRoutes = require("./comment.routes");
 const privateMessageRoutes = require("./privateMessage.routes");
 const notificationRoutes = require("./notification.routes");
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 // Configure spam-protection
 bouncer.whitelist.push('127.0.0.1'); // allow an IP address
@@ -20,6 +24,7 @@ bouncer.blocked = function (req, res, next, remaining) {
 router.get("/", (req, res) => {
     res.json("Groupomania API 1.0.0");
 });
+
 router.use("/api/auth", bouncer.block, authRoutes);
 router.use("/api/user", bouncer.block, userRoutes);
 router.use("/api/community", bouncer.block, communityRoutes);
@@ -27,5 +32,8 @@ router.use("/api/post", bouncer.block, postRoutes);
 router.use("/api/comment", bouncer.block, commentRoutes);
 router.use("/api/message", bouncer.block, privateMessageRoutes);
 router.use("/api/notification", bouncer.block, notificationRoutes);
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;

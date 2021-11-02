6e4e39a076be5dd823f1d574d74019b1c5aa938e
Bucaille Thommy <nalem.14360@gmail.com>
2021-11-02 16:02:44 +0100

    Upgraded package.json

--- app/models/post.model.js
+++ app/models/post.model.js
@@ -39,7 +39,6 @@ module.exports = function (sequelize, DataTypes) {
     Post.belongsTo(models.Community);
     Post.hasMany(models.PostLike);
     Post.hasMany(models.PostComment);
-    Post.hasMany(models.PostSeen);
     Post.hasMany(models.Post, { foreignKey: "ShareFromPostId" });
     Post.hasMany(models.PostReport);
     Post.hasMany(models.PostFile);

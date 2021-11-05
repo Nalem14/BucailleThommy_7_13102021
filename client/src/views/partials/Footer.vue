4083321207a0a013c5267f8ed0aeac9bc95415d8
Bucaille Thommy <nalem.14360@gmail.com>
2021-11-05 15:25:49 +0100

    HTML Header and Footer

--- client/src/views/partials/Footer.vue
+++ client/src/views/partials/Footer.vue
@@ -1,6 +1,14 @@
 <template>
   <footer>
-      &copy; 2021
+      <div>
+        <h2>Liens utiles</h2>
+        <ul>
+          <li><a href="#">Mentions légales</a></li>
+          <li><a href="#">Conditions générales de ventes</a></li>
+          <li><a href="mailto:contact@groupomania.tld">Nous contacter</a></li>
+        </ul>
+      </div>
+      <p>&copy; 2021 Groupomania - Tout droit réservés</p>
   </footer>
 </template>
 
--- client/src/views/partials/Header.vue
+++ client/src/views/partials/Header.vue
@@ -1,16 +1,27 @@
 <template>
   <header>
-      Header
+    <nav>
+      <div>
+        <router-link to="/">
+          <h1>Groupomania</h1>
+        </router-link>
+      </div>
+      <ul>
+        <li>
+          <router-link to="/">
+            <i class="fas fa-home"></i> Accueil
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'Header'
-}
+  name: "Header",
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>

91ad6494804e9b96026e488aa60b86e258cbe7a5
Bucaille Thommy <nalem.14360@gmail.com>
2021-11-05 15:15:48 +0100

    Added Header and Footer partials

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,18 @@
 <template>
+  <Header />
   <router-view></router-view>
+  <Footer />
 </template>
 
 <script>
+import Header from './views/partials/Header.vue'
+import Footer from './views/partials/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Header,
+    Footer
   }
 }
 </script>

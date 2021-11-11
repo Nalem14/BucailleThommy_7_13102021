1cee459b91b370a55cea6da470355121f7cb9f2b
Bucaille Thommy <nalem.14360@gmail.com>
2021-11-11 23:45:28 +0100

    remove debug

--- client/src/plugins/meta.js
+++ client/src/plugins/meta.js
@@ -1,16 +1,14 @@
 export default {
-    install: (app) => {
-      // Plugin code goes here
+  install: (app) => {
+    // Plugin code goes here
 
-      app.mixin({
-        mounted() {
-            console.log("debug", JSON.stringify(this))
-          // some logic ...
-          if('metaDatas' in this) {
-            if('title' in this.metaDatas)
-                document.title = this.metaDatas.title;
-          }
+    app.mixin({
+      mounted() {
+        // Set document title if metaDatas is specified in component
+        if ("metaDatas" in this) {
+          if ("title" in this.metaDatas) document.title = this.metaDatas.title;
         }
-      })
-    }
-  }
+      },
+    });
+  },
+};

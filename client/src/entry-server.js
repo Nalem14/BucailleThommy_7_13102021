47a4a1de9af2f7142ef5280346c02a4c87bd69e0
Bucaille Thommy <nalem.14360@gmail.com>
2022-01-02 16:37:49 +0100

    Fixed server build issue

--- client/src/entry-server.js
+++ client/src/entry-server.js
@@ -1,19 +1,21 @@
-import { createSSRApp } from 'vue'
+import { createSSRApp } from "vue";
 // server router uses a different history from the client one
-import { createMemoryHistory } from 'vue-router'
-import createRouter from './router'
-import store from './store'
-import App from './App.vue'
+import { createMemoryHistory } from "vue-router";
+import createRouter from "./router";
+import store from "./store";
+import Notifications from "@kyvg/vue3-notification";
+import App from "./App.vue";
 
 export default function () {
-  const app = createSSRApp(App)
-  const router = createRouter(createMemoryHistory())
+  const app = createSSRApp(App);
+  const router = createRouter(createMemoryHistory());
 
-  app.use(router)
-  app.use(store)
+  app.use(router);
+  app.use(store);
+  app.use(Notifications);
 
   return {
     app,
-    router
-  }
+    router,
+  };
 }
